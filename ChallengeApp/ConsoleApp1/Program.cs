b412cde748d1fb277d33c16fe0611e68d1791adc
Karol Olszewski <k.olszewskibdg@gmail.com>
2023-10-01 14:08:48 +0200

    Dzień 4, praca domowa

--- ChallengeApp/ConsoleApp1/Program.cs
+++ ChallengeApp/ConsoleApp1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex = "Kobieta";
+var age = 33;
+
+if (sex == "Kobieta" && age == 33 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex != "Kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else if (sex == "Kobieta" && age < 30)
+    Console.WriteLine("Kobieta poniżej 30 lat");
